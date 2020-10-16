723aa43e9c4cac6a4a6c7513f97ce295290cc7b1
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-10-16 18:36:25 +0100

    display existing notes on page load
    update and delete note from DOM as it occurs
    increase size of plus button

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,6 @@
 const el = (sel) => document.querySelector(sel);
 const appContainer = el('#app-container');
+const notesContainer = el('#notes-container');
 const editor = el('#editor');
 const noteForm = el('#note-form');
 const saveNoteButton = el('#save-note');
@@ -15,7 +16,57 @@ const closeEditor = () => {
   saveNoteButton.disabled = false;
   editor.style.display = 'none';
   appContainer.classList.remove('covered');
-}
+};
+
+const displayNote = (id, note) => {
+  let isNewNote = false;
+  let noteDiv = el(`#note-id-${id}`);
+  if (!noteDiv) {
+    noteDiv = document.createElement('div');
+    noteDiv.id = `note-id-${id}`;
+    noteDiv.classList.add('note');
+    isNewNote = true;
+  }
+  // If timestamp is null, assume we've gotten a brand new note.
+  // https://stackoverflow.com/a/47781432/4816918
+  noteDiv.innerHTML = `
+  <div class="note-group">
+    <span class="key">Topic:</span>
+    <span class="value">${note.topic}</span>
+  </div>
+  <div class="note-group">
+    <span class="key">Subject:</span>
+    <span class="value">${note.subject}</span>
+  </div>
+  <div class="note-group">
+    <span class="key">Details:</span>
+    <span id="details-value">${note.details.length < 150 ? note.details : (note.details.substring(0, 150) + ' ...')}</span>
+  </div>
+  <p class="date">${note.timestamp ? new Date(note.timestamp.toMillis()) : new Date()}</p>
+  <div class="button-container">
+    <button class="text-button">View</button>
+  </div>
+  `;
+  if (isNewNote) {
+    notesContainer.insertBefore(noteDiv, notesContainer.children[0]);
+    notesContainer.scrollTo(0, 0);
+  }
+};
+
+const loadNotes = () => {
+  firebase.firestore().collection('notes').orderBy('timestamp').onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === 'removed') {
+        const deletedNote = el(`#notes-container #note-id-${change.doc.id}`);
+        if (deletedNote) {
+          deletedNote.parentNode.removeChild(deletedNote);
+        }
+      } else {
+        displayNote(change.doc.id, change.doc.data());
+      }
+    });
+  });
+};
 
 detailsTextarea.addEventListener('keyup', () => {
   const numberOfLineBreaks = (detailsTextarea.value.match(/\n/g) || []).length;
@@ -29,7 +80,7 @@ detailsTextarea.addEventListener('keyup', () => {
 noteForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const formBody = Object.fromEntries(new FormData(noteForm));
-  formBody.date = new Date();
+  formBody.timestamp = firebase.firestore.FieldValue.serverTimestamp();
   saveNoteButton.disabled = true;
   firebase.firestore().collection('notes').add(formBody)
     .then(closeEditor)
@@ -37,3 +88,5 @@ noteForm.addEventListener('submit', (e) => {
       alert(error.message);
     });
 });
+
+loadNotes();
