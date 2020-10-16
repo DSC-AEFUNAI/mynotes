533d5b65a8627eb6eaa470a72dede8ffb8522194
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-10-16 16:47:44 +0100

    make details textarea to expand if input is more than 5 lines

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,6 +3,7 @@ const appContainer = el('#app-container');
 const editor = el('#editor');
 const noteForm = el('#note-form');
 const saveNoteButton = el('#save-note');
+const detailsTextarea = el('textarea#details');
 
 const createNote = () => {
   editor.style.display = 'block';
@@ -15,6 +16,15 @@ const closeEditor = () => {
   editor.style.display = 'none';
   appContainer.classList.remove('covered');
 }
+
+detailsTextarea.addEventListener('keyup', () => {
+  const numberOfLineBreaks = (detailsTextarea.value.match(/\n/g) || []).length;
+  if (numberOfLineBreaks >= 5) {
+    // minheight + lines x line-height + padding + border
+    const newHeight = (24 + numberOfLineBreaks * 18) + 2;
+    detailsTextarea.style.height = newHeight + 'px';
+  }
+});
 
 noteForm.addEventListener('submit', (e) => {
   e.preventDefault();
